65dfcab6d495aefe0e8a92b019bb1c88053254d4
SherifAbdlNaby <SherifAbdlNaby@gmail.com>
2018-03-09 23:14:31 +0200

    Security Config

---
+++ src/main/java/com/piper/valley/config/SecurityConfig.java
@@ -0,0 +1,54 @@
+package com.piper.valley.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private UserDetailsService userDetailsService;
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+				//Homepage
+				.antMatchers("/").permitAll()
+				//Static Resource
+				.antMatchers("/css/**", "/images/**", "/js/**", "/bootstrap/**").permitAll()
+				//.antMatchers("/example/**").hasAuthority("ADMIN")
+				.anyRequest().fullyAuthenticated()
+				.and()
+				.formLogin()
+				.loginPage("/login")
+				.failureUrl("/login?error")
+				.usernameParameter("username")
+				.permitAll()
+				.and()
+				.logout()
+				.logoutUrl("/logout")
+				.deleteCookies("remember-me")
+				.logoutSuccessUrl("/")
+				.permitAll()
+				.and()
+				.rememberMe();
+	}
+
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth
+				.userDetailsService(userDetailsService)
+				.passwordEncoder(new BCryptPasswordEncoder());
+	}
+
+}
