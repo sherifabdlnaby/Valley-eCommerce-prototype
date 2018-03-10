09b6d5755ac85aeedc3b91ddda774e92aa033b12
SherifAbdlNaby <SherifAbdlNaby@gmail.com>
2018-03-10 02:28:59 +0200

    Small tweak.

--- src/main/java/com/piper/valley/controllers/HomeController.java
+++ src/main/java/com/piper/valley/controllers/HomeController.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
 public class HomeController {
 
 	@RequestMapping("/")
