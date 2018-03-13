46006ba035f667229dbb962a17c427db9833d54f
mmn3mm <muhammedabdelmonim@gmail.com>
2018-03-14 01:02:25 +0200

    Admin Controller added.

---
+++ src/main/java/com/piper/valley/controllers/AdminController.java
@@ -0,0 +1,56 @@
+package com.piper.valley.controllers;
+
+import com.piper.valley.forms.AddBrandForm;
+import com.piper.valley.models.service.BrandService;
+import com.piper.valley.validators.AddBrandFormValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
+@Controller
+public class AdminController {
+    /////////////////////////*  SERVICES, REPOSITORIES AND VALIDATORS SECTION  */////////////////////////////
+    @Autowired
+    private BrandService brandService;
+    @Autowired
+    private AddBrandFormValidator brandFormValidator;
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////*  VALIDATORS BINDING SECTION  *//////////////////////////////////////
+    @InitBinder("addBrandForm")
+    public void addBrandFormInitBinder(WebDataBinder binder) {
+        binder.addValidators(brandFormValidator); //This maps the add brand form to our own validator.
+    }
+
+    @RequestMapping(value = "/add_brand", method = RequestMethod.GET)
+    public ModelAndView addBrand(@ModelAttribute("addBrandForm") AddBrandForm addBrandForm) {
+        return new ModelAndView("admin/add_brand", "addBrandForm", addBrandForm);
+    }
+    @RequestMapping(value = "/add_brand", method = RequestMethod.POST)
+    public ModelAndView addBrand(@Valid @ModelAttribute("addBrandForm")AddBrandForm addBrandForm, BindingResult bindingResult, HttpServletRequest request)
+    {
+        if(bindingResult.hasErrors())
+            return new ModelAndView("admin/add_brand","addBrandForm",addBrandForm);
+        brandService.addBrand(addBrandForm);
+        return new ModelAndView("redirect:/");
+    }
+
+
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////*  CONTROLLER ACTION  *///////////////////////////////////////////
+
+
+
+
+
+
+}
