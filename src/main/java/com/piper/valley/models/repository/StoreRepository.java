6de732b5424b675186f201b85b18cb2bd73ab519
Khaled Hamed <khaled.hamedt@gmail.com>
2018-03-14 16:04:55 +0200

    Create store repository

---
+++ src/main/java/com/piper/valley/models/repository/StoreRepository.java
@@ -0,0 +1,13 @@
+package com.piper.valley.models.repository;
+
+import com.piper.valley.models.domain.Store;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Repository
+public interface StoreRepository extends JpaRepository<Store, Long> {
+	Optional<Store> findOneById(long id);
+}
