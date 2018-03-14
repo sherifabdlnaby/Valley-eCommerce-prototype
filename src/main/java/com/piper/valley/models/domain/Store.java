bbe7b8dd5891cec9a282810c2d393db41903e46a
Khaled Hamed <khaled.hamedt@gmail.com>
2018-03-14 16:06:30 +0200

    Fix Store
    It's now a DB entity

--- src/main/java/com/piper/valley/models/domain/Store.java
+++ src/main/java/com/piper/valley/models/domain/Store.java
@@ -1,68 +1,44 @@
 package com.piper.valley.models.domain;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+
+import javax.persistence.*;
 import java.util.ArrayList;
 
-//@Entity // This tells Hibernate to make a table out of this class
+@Entity // This tells Hibernate to make a table out of this class
+@Table(name = "store")
 public class Store {
-    @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
-    private String id;
-    private String name;
-    private User owner;
-    private ArrayList<Product> products;
-    public Store() {
-        this.id="";
-        this.name="";
-        this.owner= null;
-        products=null;
-    }
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id", nullable = false, updatable = false)
+	private long id;
 
-    public Store(String id, String name, User owner) {
-        this.id = id;
-        this.name = name;
-        this.owner = owner;
-        products= new ArrayList<>();
-    }
+	@Column(name = "name", nullable = false, unique = true)
+	private String name;
 
-    public Store(String id, String name, User owner, ArrayList<Product> products) {
-        this.id = id;
-        this.name = name;
-        this.owner = owner;
-        this.products = products;
-    }
+	@Column(name = "ownerId", nullable = false, unique = true)
+	private long ownerId;
+	//private User owner;
 
-    public String getId() {
-        return id;
-    }
+	public long getId() {
+		return id;
+	}
 
-    public void setId(String id) {
-        this.id = id;
-    }
+	public void setId(long id) {
+		this.id = id;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public User getOwner() {
-        return owner;
-    }
+	public long getOwnerId() {
+		return ownerId;
+	}
 
-    public void setOwner(User owner) {
-        this.owner = owner;
-    }
-
-    public ArrayList<Product> getProducts() {
-        return products;
-    }
-
-    public void setProducts(ArrayList<Product> products) {
-        this.products = products;
-    }
+	public void setOwnerId(long ownerId) {
+		this.ownerId = ownerId;
+	}
 }
